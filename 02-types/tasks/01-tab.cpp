48ca41ad8d6e5cf5faa94b7c1147cf2a9ec2a256
macmar1906 <71896373+macmar1906@users.noreply.github.com>
2021-05-12 00:10:12 +0200

    Update 01-tab.cpp

--- 02-types/tasks/01-tab.cpp
+++ 02-types/tasks/01-tab.cpp
@@ -5,8 +5,10 @@ constexpr size_t tab_size = 100;
 int main() {
     int tab[tab_size];
 
-    for (size_t i = 0; i < tab_size; ++i) {
-        tab[i] = i;
+    for (size_t i = 0; i <= 2*tab_size-1; ++i) {
+        if (i % 2) {
+		tab[i/2] = i;
+	}
     }
 
     for (size_t i = 0; i < tab_size; ++i) {
