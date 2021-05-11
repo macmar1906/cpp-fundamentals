479f6061db206afa715fd7cf2689ee26bb3c5d95
macmar1906 <71896373+macmar1906@users.noreply.github.com>
2021-05-11 23:28:57 +0200

    Update 04-createMap.cpp

--- 04-stl-basics/tasks/04-createMap.cpp
+++ 04-stl-basics/tasks/04-createMap.cpp
@@ -2,10 +2,18 @@
 #include <list>
 #include <string>
 #include <vector>
+#include <map>
 
 // Implement createMap. It should take a vector and list and
 // return a map of merge them as keys from the vector and values from the list
-
+auto createMap(std::vector<int> vec_in,std::list<std::string> list_in){
+	std::map<int,std::string> map_out;
+	auto it=list_in.begin();
+	for(size_t i=0;i<list_in.size();++i){
+		map_out.insert({vec_in[i],*(it++)});
+	}
+	return map_out;
+};
 
 int main() {
     std::vector<int> vec{1, 2, 3, 4, 5};
